b2381980f71b91c9c3fb35e1a8ae24d7c85b442f
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-03-16 08:26:32 +0900

    enum

--- struct-enum-touple/src/main.rs
+++ struct-enum-touple/src/main.rs
@@ -1,3 +1,5 @@
+use crate::Transmission::Manual;
+
 #[derive(Debug)]
 struct KeyPress(String, char);
 
@@ -14,7 +16,37 @@ enum WebEvent {
     WEKeys(KeyPress),
 }
 
+// Declare Car struct to describe vehicle with four named fields
+#[derive(Debug)]
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+// Declare enum for Car transmission type
+#[derive(PartialEq, Debug)]
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+    let car: Car = Car {
+        color: color,
+        transmission: transmission,
+        convertible: convertible,
+        mileage: 1
+    };
+    car
+}
+
 fn main() {
+    let car = car_factory("car".to_string(), Manual, true);
+    print!("{:#?}", car);
+
     let we_load = WebEvent::WELoad(true);
     let click = MouseClick {x:100, y:250};
     let we_click = WebEvent::WEClick(click);
