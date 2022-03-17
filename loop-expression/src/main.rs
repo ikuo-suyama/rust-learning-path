e25bda4ffb407dbc6a561438332bc914cc61130a
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-03-17 22:34:22 +0900

    loop2

--- loop-expression/src/main.rs
+++ loop-expression/src/main.rs
@@ -34,4 +34,10 @@ fn main() {
     for c in b.iter() {
         println!("{}", c);
     }
+
+    let mut vals = vec![2, 3, 1, 2, 2];
+    while let Some(v @ 1) | Some(v @ 2) = vals.pop() {
+        // Prints 2, 2, then 1
+        println!("{}", v);
+    }
 }
