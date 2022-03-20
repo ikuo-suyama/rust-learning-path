0c466c186b639f8ba7a5d5aa13aaa2ba92401af5
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-03-20 20:05:11 +0900

    generics 2

--- generic-types-traits/src/main.rs
+++ generic-types-traits/src/main.rs
@@ -24,6 +24,40 @@ fn send_data_as_json2<T: AsJson>(value: &T) {
     println!("-> {}", value.as_json());
 }
 
+#[derive(Debug)]
+struct Counter {
+    length: usize,
+    count: usize,
+}
+
+impl Counter {
+    fn new(length: usize) -> Counter {
+        Counter { count: 0, length }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count <= self.length {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+struct Container<T> {
+    value: T,
+}
+
+impl <T> Container<T> {
+    fn new(value: T) -> Self {
+        Container { value }
+    }
+}
+
 fn main() {
     let clapton = Person {
         name: "Eric Clapton".to_string(),
@@ -31,4 +65,16 @@ fn main() {
     };
     send_data_as_json(&clapton);
     send_data_as_json2(&clapton);
+
+    // Iterator
+    let counter = Counter::new(6);
+    for c in counter {
+        println!("{}", c);
+    }
+
+    let powers_of_2: Vec<usize> = Counter::new(8).map(|n| 2usize.pow(n as u32)).collect();
+    assert_eq!(powers_of_2, vec![2, 4, 8, 16, 32, 64, 128, 256]);
+
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new(2).value, 2);
 }
